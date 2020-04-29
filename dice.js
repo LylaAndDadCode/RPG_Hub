32d532e8216d5b338b4db12465224a43c82f753d
LylaAndDadCode <64291561+LylaAndDadCode@users.noreply.github.com>
2020-04-28 21:06:36 -0400

    added number of die parameter to roll function

--- dice.js
+++ dice.js
@@ -1,4 +1,12 @@
-function roll(upperLimit){
-  roll2 = Math.floor(Math.random() * 10) + 1;
-  document.getElementById("demo").innerHTML = roll2;
+function roll(upperLimit, numberDie){
+  let x = 0;
+  let result = ""
+  while (x < numberDie) {
+    roll= Math.floor(Math.random() * upperLimit) + 1;
+    result = result + roll.toString() + "   ";
+    x = x +1;
+  }
+  
+    document.getElementById("demo").innerHTML = result;
+  
 }
