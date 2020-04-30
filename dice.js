77a9d3c9be0c5ce1fdf9479f0af5a65690f44622
LylaAndDadCode <64291561+LylaAndDadCode@users.noreply.github.com>
2020-04-29 21:16:44 -0400

    Add dice sum and style changes

--- dice.js
+++ dice.js
@@ -1,12 +1,15 @@
 function roll(upperLimit, numberDie){
   let x = 0;
-  let result = ""
+  let result = "";
+  let sum = 0;
   while (x < numberDie) {
-    roll= Math.floor(Math.random() * upperLimit) + 1;
-    result = result + roll.toString() + "   ";
-    x = x +1;
+    rolls= Math.floor(Math.random() * upperLimit) + 1;
+    sum = sum + rolls;
+    result = result + rolls.toString() + "   ";
+    x = x + 1;
   }
   
-    document.getElementById("demo").innerHTML = result;
-  
+  document.getElementById("result").innerHTML = result;
+  document.getElementById("sum").innerHTML = "Sum of the dice is: " + sum;
+  return;
 }
