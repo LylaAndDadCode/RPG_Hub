11ac81ff00d372a176c5abcc10756c8a236c7241
LylaAndDadCode <64291561+LylaAndDadCode@users.noreply.github.com>
2020-05-01 21:00:49 -0400

    Fixed alignment of input boxes, moved label inside box and started error check logic

--- dice.js
+++ dice.js
@@ -1,15 +1,22 @@
-function roll(upperLimit, numberDie){
+function roll(numSides, numberDie){
   let x = 0;
   let result = "";
   let sum = 0;
-  while (x < numberDie) {
-    rolls= Math.floor(Math.random() * upperLimit) + 1;
-    sum = sum + rolls;
-    result = result + rolls.toString() + "   ";
-    x = x + 1;
-  }
   
-  document.getElementById("result").innerHTML = result;
-  document.getElementById("sum").innerHTML = "Sum of the dice is: " + sum;
-  return;
+  
+  if(numSides >=2) {
+    
+    while (x < numberDie) {
+      rolls= Math.floor(Math.random() * numSides) + 1;
+      sum = sum + rolls;
+      result = result + rolls.toString() + "   ";
+      x = x + 1;
+    }
+    
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("sum").innerHTML = "Sum of the dice is: " + sum;
+    return;
+  } else {
+    alert("needs to have at least 2 sides")
+  }
 }
